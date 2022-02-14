72482358d3b8265966cbe27f7f089e596e3cc03c
frisk038 <frisktrentehuit@gmail.com>
2022-02-14 23:56:23 +0100

    colorboard as table, changing color on click

--- src/components/Board/ColorBoard.vue
+++ src/components/Board/ColorBoard.vue
@@ -3,7 +3,8 @@ import ColorRow from './ColorRow.vue'
 </script>
 
 <template>
-  <table>
+  <!-- Pass binding to sub element
+    <table>
     <ColorRow></ColorRow>
     <ColorRow></ColorRow>
     <ColorRow></ColorRow>
@@ -16,12 +17,22 @@ import ColorRow from './ColorRow.vue'
     <ColorRow></ColorRow>
     <ColorRow></ColorRow>
     <ColorRow></ColorRow>
+  </table> -->
+  <table >
+    <tbody>
+      <tr v-for="(row, idx1) in gameArr" :key="row">
+        <td v-for="(col, idx2) in row" :key="col">
+          <img v-bind:src="'src/components/assets/' +gameArr[idx1][idx2] + '-circle.svg'"/>
+        </td>
+      </tr>
+    </tbody>
   </table>
 </template>
 
 <script>
 export default {
   components: { ColorRow },
+  props: ['curColor', 'gameArr'],
   methods : {
     updateRow(curColor, curRow, curCell) {
       console.log(curColor, curRow, curCell);
@@ -30,6 +41,12 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+td {
+background-color: lightgray;
+width: 20px;
+height: 20px;
+text-align: center;
+}
 
 </style>
