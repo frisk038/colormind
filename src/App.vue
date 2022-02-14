90e7a1a8583331ed562915b0efaa7f0758fc7183
frisk038 <frisktrentehuit@gmail.com>
2022-02-14 23:56:23 +0100

    colorboard as table, changing color on click

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@ import Keyboard from './components/Keyboard.vue';
     <Header></Header>
   </header>
   <main>
-    <ColorBoard></ColorBoard>
+    <ColorBoard :curColor="curColor" :gameArr="gameArr"></ColorBoard>
     <Keyboard @guessed="toto"></Keyboard>
   </main>
 </template>
@@ -21,12 +21,19 @@ export default {
       curColor: 'grey',
       curRow: 0,
       curCell: 0,
+      gameArr: [['white', 'white', 'white', 'white'], ['white', 'white', 'white', 'white'], ['white', 'white', 'white', 'white'], ['white', 'white', 'white', 'white'],
+               ['white', 'white', 'white', 'white'], ['white', 'white', 'white', 'white'], ['white', 'white', 'white', 'white'], ['white', 'white', 'white', 'white'],
+              ['white', 'white', 'white', 'white'], ['white', 'white', 'white', 'white'], ['white', 'white', 'white', 'white'], ['white', 'white', 'white', 'white']]
     }
   },
   methods : {
     toto(curColor) {
       this.curColor = curColor;
-      ColorBoard.methods.updateRow(this.curColor, this.curRow, this.curCell);
+      this.gameArr[this.curRow][this.curCell] = curColor
+      this.curCell = (this.curCell + 1) % 4
+      if (this.curCell == 0) {
+        this.curRow = (this.curRow + 1) % 12
+      }
     }
   },
 }
