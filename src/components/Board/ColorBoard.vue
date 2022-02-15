e2d4ad8f64ff05a91d3695354a9da95e6874d446
frisk038 <frisktrentehuit@gmail.com>
2022-02-16 00:20:05 +0100

    fixing assets path resolver in production

--- src/components/Board/ColorBoard.vue
+++ src/components/Board/ColorBoard.vue
@@ -1,28 +1,14 @@
 <script setup>
 import ColorRow from './ColorRow.vue'
+import WhiteImg from '../assets/white-circle.svg'
 </script>
 
 <template>
-  <!-- Pass binding to sub element
-    <table>
-    <ColorRow></ColorRow>
-    <ColorRow></ColorRow>
-    <ColorRow></ColorRow>
-    <ColorRow></ColorRow>
-    <ColorRow></ColorRow>
-    <ColorRow></ColorRow>
-    <ColorRow></ColorRow>
-    <ColorRow></ColorRow>
-    <ColorRow></ColorRow>
-    <ColorRow></ColorRow>
-    <ColorRow></ColorRow>
-    <ColorRow></ColorRow>
-  </table> -->
   <table >
     <tbody>
       <tr v-for="(row, idx1) in gameArr" :key="row">
         <td v-for="(col, idx2) in row" :key="col">
-          <img v-bind:src="'src/components/assets/' +gameArr[idx1][idx2] + '-circle.svg'"/>
+          <img :src=getImages(gameArr[idx1][idx2]) />
         </td>
       </tr>
     </tbody>
@@ -32,10 +18,33 @@ import ColorRow from './ColorRow.vue'
 <script>
 export default {
   components: { ColorRow },
-  props: ['curColor', 'gameArr'],
+  props: ['gameArr'],
   methods : {
-    updateRow(curColor, curRow, curCell) {
-      console.log(curColor, curRow, curCell);
+    getImages(color) {
+      console.log(color)
+      switch (color) {
+        case 'white':
+          const whiteUrl = new URL('../assets/white-circle.svg', import.meta.url)
+          return whiteUrl;
+        case 'black':
+          const blackUrl = new URL('../assets/black-circle.svg', import.meta.url)
+          return blackUrl
+        case 'green':
+          const greenUrl = new URL('../assets/green-circle.svg', import.meta.url)
+          return greenUrl
+        case 'brown':
+          const brownUrl = new URL('../assets/brown-circle.svg', import.meta.url)
+          return brownUrl
+        case 'yellow':
+          const yellowUrl = new URL('../assets/yellow-circle.svg', import.meta.url)
+          return yellowUrl 
+        case 'orange':
+          const orangeUrl = new URL('../assets/orange-circle.svg', import.meta.url)
+          return orangeUrl 
+        case 'purple':
+          const purpleUrl = new URL('../assets/purple-circle.svg', import.meta.url)
+          return purpleUrl 
+      }
     }
   }
 }
